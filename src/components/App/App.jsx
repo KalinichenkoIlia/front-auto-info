8859e68f1657c8d6b7fae241fd1b0a9e76551bf6
TreworReznik <kalinichenko.ilya@gmail.com>
2024-07-29 16:48:35 +0300

    new

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,15 @@
 import React, {useEffect, useState} from "react";
 import style from '../../styles/App/App.modules.css'
 
+const tg = window.Telegram.WebApp
+
 function App() {
     return (
         <div>
-привет
+        <span>{tg.initDataUnsafe?.user?.username}</span>
+            <span>{tg.initDataUnsafe?.user?.id}</span>
+            <span>{tg.initDataUnsafe?.user?.id}</span>
+            <span>{tg.initDataUnsafe?.user}</span>
         </div>
     );
 }
