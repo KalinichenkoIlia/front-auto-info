6cda85dd2b9349c8460bdf6e813e0334d19860d0
TreworReznik <kalinichenko.ilya@gmail.com>
2024-07-29 21:05:48 +0300

    new test

--- src/components/Web/Web.jsx
+++ src/components/Web/Web.jsx
@@ -5,12 +5,13 @@ import * as styles from '../../styles/Web/Web.modules.css'
 function Web() {
     const tg = window.Telegram.WebApp
     console.log(tg, 'tg')
+
     return (
         <>
 
             <div className={styles.wrapper} key={tg.initDataUnsafe?.user} >{tg.initDataUnsafe?.user?.username}</div>
             <div className={styles.wrapper} key={tg.initDataUnsafe?.user?.id}>{tg.initDataUnsafe?.user?.id}</div>
-            <img src={tg.initDataUnsafe?.user?.photo}/>
+            <img key={tg.initDataUnsafe.user.photo_url} src={tg.initDataUnsafe.user.photo_url}/>
         </>
     );
 }
